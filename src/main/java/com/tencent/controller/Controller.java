87f8e2d466c5c01b6f325d6a95dccf450568176e
yishuang <164497083@qq.com>
2019-07-24 00:02:19 +0800

    信息提取

--- src/main/java/com/tencent/controller/Controller.java
+++ src/main/java/com/tencent/controller/Controller.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author : ys
@@ -16,9 +16,23 @@ import java.util.Map;
 @RestController
 public class Controller {
 	@RequestMapping("/payload")
-	public Map fun1(@RequestBody HashMap map) {
+	public String fun1(@RequestBody HashMap<String, Object> map) {
 		String str = JSON.toJSONString(map);
 		System.out.println(str);
-		return map;
+
+		ArrayList<String> list = new ArrayList<>();
+		String author = "";
+		ArrayList<HashMap<String, Object>> objs = (ArrayList<HashMap<String, Object>>) (map.get("commits"));
+		int len = objs.size();
+		for (int i = 0; i < len; i++) {
+			HashMap<String, Object> commit = objs.get(i);
+			list.add((String) commit.get("message"));
+//			String message = (String) commit.get("message");
+			HashMap<String, String> authinfo = (HashMap<String, String>) commit.get("author");
+			author = authinfo.get("name");
+		}
+		String msg = String.format("%s推送了%d次提交，请查看\ncommit log:%s", author, len, list);
+		System.out.println(msg);
+		return "ok";
 	}
 }
